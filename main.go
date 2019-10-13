6a5a89d04ee517ba0986ed962316deab8943a114
Junpei-Nakasone <nakasone.junpei0719@gmail.com>
2019-10-13 22:46:24 +0900

    fix bitflyer.go

--- main.go
+++ main.go
@@ -11,5 +11,7 @@ import (
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
 	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
-	fmt.Println(apiClient.GetBalance())
+	ticker, _ := apiClient.GetTicker("BTC_USD")
+	fmt.Println(ticker)
+	fmt.Println(ticker.DateTime())
 }
