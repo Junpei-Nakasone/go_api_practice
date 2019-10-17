e663e6d9569dba7a23742a1a587aacebcbaece52
Junpei-Nakasone <nakasone.junpei0719@gmail.com>
2019-10-17 22:11:31 +0900

    add candle.go

---
+++ app/models/candle.go
@@ -0,0 +1,66 @@
+package models
+
+import (
+	"fmt"
+	"time"
+)
+
+type Candle struct {
+	ProductCode string
+	Duration    time.Duration
+	Time        time.Time
+	Open        float64
+	Close       float64
+	High        float64
+	Low         float64
+	Volume      float64
+}
+
+func NewCandle(productCode string, duration time.Duration, timeDate time.Time, open, close, high, low, volume float64) *Candle {
+	return &Candle{
+		productCode,
+		duration,
+		timeDate,
+		open,
+		close,
+		high,
+		low,
+		volume,
+	}
+}
+
+func (c *Candle) TableName() string {
+	return GetCandleTableName(c.ProductCode, c.Duration)
+}
+
+// キャンドルを最初に作る時はCREATEする
+func (c *Candle) Create() error {
+	cmd := fmt.Sprintf("INSERT INTO %s (time, open, close, high, low, volume) VALUES (?, ?, ?, ?, ?, ?)", c.TableName())
+	_, err := DbConnection.Exec(cmd, c.Time.Format(time.RFC3339), c.Open, c.Close, c.High, c.Low, c.Volume)
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+// すでにキャンドルがある場合はUPDATEして形を変えていく
+func (c *Candle) Save() error {
+	cmd := fmt.Sprintf("UPDATE %s SET open = ?, close = ?, high = ?, low = ?, volume = ? WHERE time = ?", c.TableName())
+	_, err := DbConnection.Exec(cmd, c.Open, c.Close, c.High, c.Low, c.Volume, c.Time.Format(time.RFC3339))
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+func GetCandle(productCode string, duration time.Duration, dateTime time.Time) *Candle {
+	tableName := GetCandleTableName(productCode, duration)
+	cmd := fmt.Sprintf("SELECT time, open, close, high, low, volume FROM %s WHERE time = ?", tableName)
+	row := DbConnection.QueryRow(cmd, dateTime.Format(time.RFC3339))
+	var candle Candle
+	err := row.Scan(&candle.Time, &candle.Open, &candle.Close, &candle.High, &candle.Low, &candle.Volume)
+	if err != nil {
+		return nil
+	}
+	return NewCandle(productCode, duration, candle.Time, candle.Open, candle.Close, candle.High, candle.Low, candle.Volume)
+}
