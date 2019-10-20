c66c779a81e16f9abaea2be99f2ee5ce29880bd8
Junpei-Nakasone <nakasone.junpei0719@gmail.com>
2019-10-20 22:12:11 +0900

    add webserver.go

--- app/controllers/streamdata.go
+++ app/controllers/streamdata.go
@@ -19,20 +19,22 @@ func StreamIngestionData() {
 	// goroutineを回してリアルタイムでAPIから情報を取得する
 	go apiClient.GetRealTimeTicker(config.Config.ProductCode, tickerChannl)
 
-	for ticker := range tickerChannl {
-		// ログにリアルタイムで取得しているデータを出力する
-		log.Printf("action=StreamIngestionData, %v", ticker)
+	go func() {
+		for ticker := range tickerChannl {
+			// ログにリアルタイムで取得しているデータを出力する
+			log.Printf("action=StreamIngestionData, %v", ticker)
 
-		// 下記のfor分はログ出力とは関係ない、キャンドルを作るためのもの？
-		for _, duration := range config.Config.Durations {
-			isCreated := models.CreateCandleWithDuration(ticker, ticker.ProductCode, duration)
+			// 下記のfor分はログ出力とは関係ない、キャンドルを作るためのもの？
+			for _, duration := range config.Config.Durations {
+				isCreated := models.CreateCandleWithDuration(ticker, ticker.ProductCode, duration)
 
-			// 下記のようにログを出すとfor文で３回出力される
-			// log.Println("isCreated is working")
+				// 下記のようにログを出すとfor文で３回出力される
+				// log.Println("isCreated is working")
 
-			if isCreated == true && duration == config.Config.TradeDuration {
-				// TODO
+				if isCreated == true && duration == config.Config.TradeDuration {
+					// TODO
+				}
 			}
 		}
-	}
+	}()
 }
