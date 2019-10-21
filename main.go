491bfd185721e6ddaadca07d341a2606f4d946f5
Junpei-Nakasone <nakasone.junpei0719@gmail.com>
2019-10-21 22:46:54 +0900

    add dfcandle.go

--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/gotrading/app/controllers"
-	"github.com/gotrading/app/models"
 	"github.com/gotrading/config"
 	"github.com/gotrading/utils"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
-	fmt.Println(models.DbConnection)
 	controllers.StreamIngestionData()
 	controllers.StartWebServer()
 }
